a9d5d98d89a08ef60da22d54ac9a0f68328bb71f
aibrahimovic <aibrahimov3@etf.unsa.ba>
2015-03-17 22:56:18 +0100

    Register2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Test3::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :domain         => '',
