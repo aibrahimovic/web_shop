2fcba1fbe7a89890d9c9100c57ee33b005385057
aibrahimovic <aibrahimov3@etf.unsa.ba>
2015-04-14 16:39:21 +0200

    Adding product 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,12 +99,15 @@ Test3::Application.configure do
     :domain => 'yourdomain.com', # your domain to identify your server when connecting
   }
 
+
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_host_name: 's3-eu-west-1.amazonaws.com',
+    s3_credentials: {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    bucket: ENV['S3_BUCKET_NAME']
   }
 }
 end
