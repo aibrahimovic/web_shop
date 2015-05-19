0c524dd03c966641cc12d7fcdf6079b7f5afc139
aibrahimovic <aibrahimov3@etf.unsa.ba>
2015-05-19 11:18:53 +0200

    Add image error

--- app/models/image.rb
+++ app/models/image.rb
@@ -13,7 +13,7 @@ class Image < ActiveRecord::Base
                   	  :default_url => "/images/:style/missing.png"
                     }
   
-  #validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   #validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
   #do_not_validate_attachment_file_type :avatar
 
